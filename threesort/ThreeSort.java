634e2838e6e8eb3c5133a379ef5a6b68ebe30c80
iantheninja <munene.ian@live.com>
2016-01-25 09:41:27 +0300

    third largest toy problem added

--- threesort/ThreeSort.java
+++ threesort/ThreeSort.java
@@ -9,7 +9,7 @@
 // Given three numbers as input, find the min, middle and max of the three.
 // Return an array of the numbers in ascending order.
 // Hint: to easily see your output, import java.util.Arrays
-// and use the Arrays.toString(array) method to convert the Array to a string 
+// and use the Arrays.toString(array) method to convert the Array to a string
 // and then log it using System.out.println();
 
 // Example:
