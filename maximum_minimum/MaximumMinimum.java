5617682b8de508fa2b69e96ad1e1c8718a990315
kimaiga <nelsonkimaiga@aol.com>
2016-02-08 08:33:04 +0300

    fixed typo

--- maximum_minimum/MaximumMinimum.java
+++ maximum_minimum/MaximumMinimum.java
@@ -2,7 +2,7 @@
 // Given an array of integers, find the maximum and minimum of this array.
 // create a variable to hod the maximum
 // create a variable to hold the minimum
-// take in the array of numbeers
+// take in the array of numbers
 // begin to loop through them
 // put the lowest in its array 
 // put the highest in its array
