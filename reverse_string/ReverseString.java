d3d6f48b0151598bc6d51413947ddf202758130b
martin <martow17@gmail.com>
2016-02-08 18:16:19 +0300

    add git

--- reverse_string/ReverseString.java
+++ reverse_string/ReverseString.java
@@ -3,4 +3,12 @@
 
 public class ReverseString{
 	//Code goes here:
+private static String myReverse(String str) {
+    String reverse = "";
+    int length = str.length();
+    for( int i = length - 1 ; i >= 0 ; i-- ) {
+       reverse = reverse + str.charAt(i);
+    }
+    return reverse;
+}
 }
