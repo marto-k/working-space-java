7ba51f7836bc4fe606ce46919440eee271c1fca3
kimaiga <nelsonkimaiga@aol.com>
2016-02-08 08:32:01 +0300

    maximum minimum toy problem

---
+++ maximum_minimum/MaximumMinimum.java
@@ -0,0 +1,9 @@
+// Using java solve the problem below:
+// Given an array of integers, find the maximum and minimum of this array.
+// create a variable to hod the maximum
+// create a variable to hold the minimum
+// take in the array of numbeers
+// begin to loop through them
+// put the lowest in its array 
+// put the highest in its array
+// output the highest then output the lowest
