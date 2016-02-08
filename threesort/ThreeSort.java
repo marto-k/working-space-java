90f6deb63317a0558539b09f463aaf5fe5592f40
martin <martow17@gmail.com>
2016-02-08 18:19:58 +0300

    add git

--- threesort/ThreeSort.java
+++ threesort/ThreeSort.java
@@ -15,8 +15,24 @@
 // Example:
 // ThreeSort.threeSort(9,4,6); // [4,6,9]
 // ThreeSort.threeSort(3,2,1); // [1,2,3]
+public class ThreeSort { 
+    public static void main(String[] args) { 
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
 
-public class ThreeSort {
+        // compute stats
+        int min    = Math.min(a, Math.min(b, c));
+        int max    = Math.max(a, Math.max(b, c));
+        int median = a + b + c - min - max;
 
+        // print stats
+        System.out.println(min);
+        System.out.println(median);
+        System.out.println(max);
+    }
 }
+
+
+
 
