0f9e7768a1ec54d22486382faeea0309d2950b34
martin <martow17@gmail.com>
2016-02-08 18:36:22 +0300

    add git

---
+++ Algorithms/src/LargestNum/Largestnum.java
@@ -0,0 +1,15 @@
+package LargestNum;
+
+public class Largestnum {
+
+	public boolean largestNumOfThree(int num1,int num2,int num3){
+		if (num1>num3){
+			return false;
+			}
+			else if(num3<num2){
+				return true;
+				}
+		return false;
+		}
+	}
+
