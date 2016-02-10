c2bf1cdc5219789c9e584d478aa698662d63c1cc
martin <martow17@gmail.com>
2016-02-10 14:03:31 +0300

    add git

--- combine_string/combineString.java
+++ combine_string/combineString.java
@@ -5,3 +5,27 @@
 // Str3 is said to be a shuffle of str1 and str2 if it can be formed by interleaving the characters of str1 and str2 in a way that maintains the left to right ordering of the characters from each string. 
 // For example, given str1="abc" and str2="def", str3="dabecf" is a valid shuffle since it preserves the character ordering of the two strings. 
 // So, given these 3 strings write a function that detects whether str3 is a valid shuffle of str1 and str2.
+public class isShuffle{
+ public static void main(string[]Args){
+def isShuffle(str1, str2, str3):
+    if len(str1)+len(str2)!=len(str3):
+        return False
+ 
+    if not str1 or not str2 or not str3:
+        if str1+str2==str3:
+            return True
+        else:
+            return False
+ 
+    if str1[0]!=str3[0] and str2[0]!=str3[0]:
+        return False
+ 
+    if str1[0]==str3[0] and isShuffle(str1[1:], str2, str3[1:]):
+            return True
+    if str2[0]==str3[0] and isShuffle(str1, str2[1:], str3[1:]):
+            return True
+ 
+    return False
+    }
+    
+}
--- next_highest/nextHighestNumber.java
+++ next_highest/nextHighestNumber.java
@@ -1,3 +1,25 @@
 // Given a number, find the next higher number using only the digits 
 //in the given number. For example if the given number is 1508, 
 //the next higher number with same digits is 1805.
+import java.util.Arrays;
+
+public class HigherNumber{
+
+
+
+	    
+	public static void main(String [] Args){
+		int num = 1508;
+		char[] x = String.valueOf(num).toCharArray();
+
+		Arrays.sort(x);
+
+		for (int i = x.length-1;i>=0; i--){
+		    System.out.print (x[i]);
+		    
+		    }
+		    
+		}
+		    
+
+}
